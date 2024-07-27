8c069d91409603e3b9ad6f27811698bfffabb526
TANVIR0222 <tanvirislam3912@gmail.com>
2024-07-27 22:53:01 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 
+
+mongoose.connect(process.env.MONGODB);
 const port = process.env.PORT || 5000
 
 app.listen(port, () =>{
     console.log(`server is running ${port}`);
 })
+
+// tanvirislam3912 4VzQttBOaY1eaTmv
+
+// 
