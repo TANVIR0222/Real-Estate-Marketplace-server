3a79c284b004d24b0f8444c8f9a3f69a7b236630
TANVIR0222 <tanvirislam3912@gmail.com>
2024-07-27 23:04:25 +0600

    add email

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => console.log("mongoBD connect "))
+  .catch((error) => console.log(error));
+const port = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGODB);
-const port = process.env.PORT || 5000
-
-app.listen(port, () =>{
-    console.log(`server is running ${port}`);
-})
-
-// tanvirislam3912 4VzQttBOaY1eaTmv
-
-// 
+app.listen(port, () => {
+  console.log(`server is running ${port}`);
+});
