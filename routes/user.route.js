53c283f43a5c2ef6411d499844ce2886edc50f17
TANVIR0222 <tanvirislam3912@gmail.com>
2024-07-31 22:35:57 +0600

    verify Tooken

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { test } from "../Controllers/user.controller.js";
+import { deleteUser, test, updateUser } from "../Controllers/user.controller.js";
+// import  verifyToken  from "../utils/verifyUser.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router =express.Router();
 
 router.get('/test',test)
+router.post('/update/:id', verifyToken , updateUser)
+router.delete('/deleted/:id', verifyToken , deleteUser)
+
 
 export default router;
